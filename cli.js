0476bb5acdcc8e3715dfd2aef00c7c6eac5af77d
Andy Gonzalez <gonzalezandy21@gmail.com>
2025-12-01 18:09:24 -0500

    Added Logic To Create Project Folder
    Creates project folder with given name outside of both clientapp and api

--- cli.js
+++ cli.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
-
+    const fs = require('node:fs');
+    const path = require('node:path');
     const { execSync } = require('child_process');
     const args = process.argv.slice(2);
+    const folderName = args[0] ?? Error("Expected project name!");
 
     console.log("Hello! Creating Custom EZVN Project!");
     console.log("Arguments passed:", args);
 
-    try {
-    const viteOut = execSync(`npm create vite@latest ${args[1]?args[1]+"-clientapp":"clientapp"} -- --template ${args[0]??"react"}`);
-    console.log(`Vite Out: ${viteOut.toString()}`);
-    const netOut = execSync(`dotnet new webapi -n ${args[1]?args[1]+"-api":"api"} `);
-    console.log(`ASP.NET API Out: ${netOut}`)
-    } catch (error) {
-    console.error(`Error executing command: ${error.message}`);
-    }
+    const currentDirectory = process.cwd();
+    const newFolderPath = path.join(currentDirectory, folderName);
 
-    // Here, you would add your project scaffolding logic.
-    // This could involve:
-    // - Prompting the user for project details (e.g., project name, features)
-    // - Copying template files from a 'template' directory within your package
-    // - Installing dependencies with 'npm install' or 'yarn install'
-    // - Initializing a Git repository
+    fs.access(newFolderPath, (error) => {
+    if (error) {
+        // Creates folder if it doesn't exist
+        fs.mkdir(newFolderPath, { recursive: true }, (err) => {
+        if (err) {
+            console.error('Error creating folder:', err);
+        } else {
+            console.log(`Folder '${folderName}' created successfully!`);
+            process.chdir(newFolderPath);
+            try {
+            const viteOut = execSync(`npm create vite@latest ${args[0]?args[0]+"-clientapp":"clientapp"} -- --template ${args[1]??"react"}`);
+            console.log(`Vite Out: ${viteOut.toString()}`);
+            const netOut = execSync(`dotnet new webapi -n ${args[0]?args[0]+"-api":"api"} `);
+            console.log(`ASP.NET API Out: ${netOut}`)
+            } catch (error) {
+            console.error(`Error executing command: ${error.message}`);
+            }
+        }
+        });
+    } else {
+        console.log(`Folder '${folderName}' already exists.`);
+    }
+    });
